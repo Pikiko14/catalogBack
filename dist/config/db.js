739971ad05bb55c17442446ef29292a737349a07
olasanmi <maalhequi95@gmail.com>
2024-02-24 14:22:50 -0500

    change dot env config

--- dist/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const dbConnect = async () => {
-    const DB_URI = process.env.DB_URI;
-    await (0, mongoose_1.connect)(DB_URI);
-};
-exports.default = dbConnect;
